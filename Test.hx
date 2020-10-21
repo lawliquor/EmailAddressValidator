37fe5cdf2e26b8688e1443048a02bac01b64d52a
lawliquor <70189980+lawliquor@users.noreply.github.com>
2020-10-21 16:00:18 +0900

    Update Test.hx

--- Test.hx
+++ Test.hx
@@ -2,6 +2,7 @@ import sneaker.assertion.Asserter.assert;
 
 class Test {
 	static function main() {
+		// trace(EmailAddressValidator.isEmail('simple@example.com'));
 		assertEmail();
 	}
 
@@ -45,49 +46,36 @@ class Test {
 			'"""@example.org'
 		];
 		var rejectDomainMock:Array<String> = [
-			'simple@example.com', 'very.common@example.com', 'other.email-with-hyphen@example.com', 'fully-qualified-domain@example.com',
-			'user.name+tag+sorting@example.com', 'x@example.com', 'example-indeed@strange-example.com', 'mailhost!username@example.org',
-			'user%example.com@example.org', '" "@example.org', '"john..doe"@example.org', 'disposable.style.email.with+symbol@example.com'
+			               'simple@example.com', 'very.common@example.com', 'other.email-with-hyphen@example.com',
+			          'fully-qualified-domain@example.com',
+			'user.name+tag+sorting@example.com',           'x@example.com',  'example-indeed@strange-example.com',
+			               'mailhost!username@example.org',
+			     'user%example.com@example.org',         '" "@example.org',             '"john..doe"@example.org',
+			'disposable.style.email.with+symbol@example.com'
 		];
 
 		for (v in trueMock) {
-			assert(EmailAddressValidator.isEmail(v, {
-				domain: [],
-				local: []
-			}) == true);
+			assert(EmailAddressValidator.isEmail(v) == true);
 			trace("case1 " + v + " == true : passed");
 		}
 		for (v in falseMock) {
-			assert(EmailAddressValidator.isEmail(v, {
-				domain: [],
-				local: []
-			}) == false);
+			assert(EmailAddressValidator.isEmail(v) == false);
 			trace("case2 " + v + " == false : passed");
 		}
 		for (v in trueIPMock) {
-			assert(EmailAddressValidator.isEmail(v, {
-				domain: [],
-				local: []
-			},true) == true);
+			assert(EmailAddressValidator.isEmail(v) == true);
 			trace("case3 " + v + " == true : passed");
 		}
 		for (v in falseIPMock) {
-			assert(EmailAddressValidator.isEmail(v, {
-				domain: [],
-				local: []
-			},true) == false);
+			assert(EmailAddressValidator.isEmail(v) == false);
 			trace("case4 " + v + " == false : passed");
 		}
 		for (v in trueIPMock) {
-			assert(EmailAddressValidator.isEmail(v, {
-				domain: [],
-				local: []
-			}) == false);
+			assert(EmailAddressValidator.isEmail(v, false) == false);
 			trace("case5 " + v + " == false : passed");
 		}
 		for (v in rejectLocalMock) {
 			assert(EmailAddressValidator.isEmail(v, {
-				domain: [],
 				local: ["+", "!", "%", " ", '"']
 			}) == false);
 			trace("case6 " + v + " == false : passed");
@@ -95,8 +83,7 @@ class Test {
 
 		for (v in rejectDomainMock) {
 			assert(EmailAddressValidator.isEmail(v, {
-				domain: ["o"],
-				local: []
+				domain: ["o"]
 			}) == false);
 			trace("case7 " + v + " == false : passed");
 		}
